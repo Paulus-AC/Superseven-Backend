97ce5a8826c4133aae690a1a1c366147df61079b
paulus <pauljairuscaberte34@gmail.com>
2025-07-02 20:22:21 +0800

    alter unavailable dates table.

---
+++ database/migrations/2025_07_02_201859_alter_unavailable_dates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('unavailable_dates', function (Blueprint $table) {
+            $table->dropColumn('reason');
+            $table->string('date')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('unavailable_dates', function (Blueprint $table) {
+            $table->string('reason')->nullable();
+            $table->date('date')->change();
+        });
+    }
+};
