318b9e0b7589c7fbf9853578444f154d9ac228d5
paulus <pauljairuscaberte34@gmail.com>
2026-01-25 07:50:10 +0800

    added total amount to booking resource.

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -37,6 +37,7 @@ class BookingResource extends JsonResource
             'deliverable_status' => Booking::DELIVERABLE_STATUS[$this->deliverable_status],
             'package' => $this->package->package_name,
             'add_ons' => AddonResource::collection($this->addOns),
+            'total_amount' => $this->billing ? $this->billing->total_amount : 0,
             'has_feedback' => $this->has_feedback,
             'has_full_payment' => $this->has_full_payment,
             'feedback' => $this->feedback ? $this->feedback->feedback_details : null,
